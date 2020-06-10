d914fa0d79bec799259ed6f0809237e37c10e615
shockliang <shockwater@gmail.com>
2020-06-11 00:09:24 +0800

    Reusable state updates.

--- starter/src/screens/SquareScreen.js
+++ starter/src/screens/SquareScreen.js
@@ -2,29 +2,45 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import ColorCounter from "../components/ColorCounter";
 
-const COLOR_INCREMENT = 10;
+const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
+  const setColor = (color, change) => {
+    // color === 'red', 'green', 'blue'
+    // change === +COLOR_INCREMENT or -COLOR_INCREMENT
+    switch (color) {
+      case 'red':
+        !(red + change > 255 || red + change < 0) ? setRed(red + change) : null;
+        break;
+      case 'green':
+        !(green + change > 255 || green + change < 0) ? setGreen(green + change) : null;
+        break;
+      case 'blue':
+        !(blue + change > 255 || blue + change < 0) ? setBlue(blue + change) : null;
+        break;
+    }
+  };
+
   return (
     <View>
       <ColorCounter
         color={"Red"}
-        onIncrease={() => setRed(red + COLOR_INCREMENT)}
-        onDecrease={() => setRed(red - COLOR_INCREMENT)}
+        onIncrease={() => setColor('red', COLOR_INCREMENT)}
+        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
       />
       <ColorCounter
         color={"Green"}
-        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        onIncrease={() => setColor('green', COLOR_INCREMENT)}
+        onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
       />
       <ColorCounter
         color={"Blue"}
-        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+        onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
       />
       <View style={{height: 150, width: 150, backgroundColor: `rgb(${red},${green},${blue})`}}/>
     </View>
