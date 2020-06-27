10e50c4d7a3de2064a42ce58eefcedc102d7e628
shockliang <shockwater@gmail.com>
2020-06-27 15:18:32 +0800

    A touch more styling.

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -5,7 +5,7 @@ import {Feather} from '@expo/vector-icons';
 const SearchBar = () => {
   return (
     <View style={styles.backgroundStyle}>
-      <Feather name="search" size={30}/>
+      <Feather name="search" style={styles.iconStyle}/>
       <TextInput
         style={styles.inputStyle}
         placeholder={"Search"}
@@ -20,12 +20,17 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
+    marginTop: 15,
     flexDirection: 'row',
   },
   inputStyle: {
-    borderColor: 'black',
-    borderWidth: 1,
     flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15,
   }
 });
 
