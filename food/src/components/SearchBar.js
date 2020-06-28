08e7884f0f125c79df7c2e4d0a1cf8e9a7507609
shockliang <shockwater@gmail.com>
2020-06-28 15:05:01 +0800

    Fixing typo.

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, TextInput, StyleSheet} from 'react-native';
 import {Feather} from '@expo/vector-icons';
+import yelp from '../api/yelp';
 
 const SearchBar = ({term, onTermChange, onTermSubmit}) => {
   return (
