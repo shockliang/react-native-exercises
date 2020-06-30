cf4b2adda0fad372af88529d9b9079782b4833a5
shockliang <shockwater@gmail.com>
2020-06-30 09:54:26 +0800

    Error handling.

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -6,16 +6,22 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    }
+    catch (e) {
+      setErrorMessage('Something wrong. Please try again');
+    }
   }
 
   return (
@@ -25,6 +31,7 @@ const SearchScreen = () => {
         onTermChange={setTerm}
         onTermSubmit={searchApi}
       />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
