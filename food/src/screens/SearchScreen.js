1dcc6baec8cd7b5df86694e850e06515bbc8c1fe
shockliang <shockwater@gmail.com>
2020-07-02 00:41:27 +0800

    Grouping results.

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -2,12 +2,17 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
-import SectionList from "react-native-web";
 import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price;
+    })
+  };
 
   return (
     <View>
@@ -18,9 +23,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList title={"Cost Effective"}/>
-      <ResultsList title={"Bit Pricier"}/>
-      <ResultsList title={"Big Spender"}/>
+      <ResultsList title={"Cost Effective"} results={filterResultsByPrice('$')}/>
+      <ResultsList title={"Bit Pricier"} results={filterResultsByPrice('$$')}/>
+      <ResultsList title={"Big Spender"} results={filterResultsByPrice('$$$')}/>
     </View>
   );
 };
