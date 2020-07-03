3ba60315992786941e9d897272a73db927fd291f
shockliang <shockwater@gmail.com>
2020-07-04 00:54:43 +0800

    Showing additional info.

--- food/src/components/ResultsDetail.js
+++ food/src/components/ResultsDetail.js
@@ -4,11 +4,25 @@ import {View, Image, Text, StyleSheet} from 'react-native';
 const ResultDetail = ({result}) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image
+        style={styles.imageStyle}
+        source={{uri: result.image_url}}
+      />
+      <Text style={styles.nameStyle}>{result.name}</Text>
+      <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
     </View>
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageStyle: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+  },
+  nameStyle: {
+    fontWeight: 'bold',
+  }
+});
 
 export default ResultDetail;
