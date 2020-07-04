1cf0caa92e92d9771f3e1a926a112de843556158
shockliang <shockwater@gmail.com>
2020-07-04 12:36:54 +0800

    Hiding scroll bars.

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
 import ResultsList from "../components/ResultsList";
@@ -23,9 +23,11 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList title={"Cost Effective"} results={filterResultsByPrice('$')}/>
-      <ResultsList title={"Bit Pricier"} results={filterResultsByPrice('$$')}/>
-      <ResultsList title={"Big Spender"} results={filterResultsByPrice('$$$')}/>
+      <ScrollView>
+        <ResultsList title={"Cost Effective"} results={filterResultsByPrice('$')}/>
+        <ResultsList title={"Bit Pricier"} results={filterResultsByPrice('$$')}/>
+        <ResultsList title={"Big Spender"} results={filterResultsByPrice('$$$')}/>
+      </ScrollView>
     </View>
   );
 };
