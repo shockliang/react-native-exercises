a95fcdd5d070dfa99609ae158761882a62faa967
shockliang <shockwater@gmail.com>
2020-07-04 12:46:31 +0800

    Spacing on the search bar.

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -22,7 +22,6 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
       <ScrollView>
         <ResultsList title={"Cost Effective"} results={filterResultsByPrice('$')}/>
         <ResultsList title={"Bit Pricier"} results={filterResultsByPrice('$$')}/>
