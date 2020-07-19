018727c24348b8f9e736f8677063bc67736a894b
shockliang <shockwater@gmail.com>
2020-07-19 13:47:12 +0800

    Adding create screen.

---
+++ blog/src/screens/CreateScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const CreateScreen = () => {
+  return (
+    <View>
+      <Text>Create screen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
