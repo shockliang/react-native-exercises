82e3cfaee3714c635a3ef1407de32c0bb078fea0
shockliang <shockwater@gmail.com>
2020-07-21 00:36:49 +0800

    Saving a new post.

--- blog/src/screens/CreateScreen.js
+++ blog/src/screens/CreateScreen.js
@@ -5,13 +5,18 @@ import {Context} from "../context/BlogContext";
 const CreateScreen = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const {addBlogPost} = useContext(Context);
+
   return (
     <View>
       <Text style={styles.label}>Enter Title:</Text>
       <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)}/>
       <Text style={styles.label}>Enter Content:</Text>
       <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
-      <Button title={"Add Blog Post"} />
+      <Button
+        title={"Add Blog Post"}
+        onPress={() => addBlogPost(title, content)}
+      />
     </View>
   )
 };
