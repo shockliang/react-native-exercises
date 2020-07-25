b0a192835fcabcd88b5b0cdf0cb2652cf1880d56
shockliang <shockwater@gmail.com>
2020-07-26 01:02:09 +0800

    Extracting form logic.

--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -1,38 +1,16 @@
-import React, {useState, useContext} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import React, {useContext} from 'react';
+import {StyleSheet} from 'react-native';
 import {Context} from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({navigation}) => {
   const id = navigation.getParam('id');
   const {state} = useContext(Context);
   const blogPost = state.find(blogPost => blogPost.id === id);
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
 
-  return (
-    <View>
-      <Text style={styles.label}>Enter Title:</Text>
-      <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)}/>
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
-    </View>
-  )
+  return <BlogPostForm/>
 };
 
-const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 15,
-    padding: 5,
-    margin: 5
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 10,
-    marginLeft: 5
-  }
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
