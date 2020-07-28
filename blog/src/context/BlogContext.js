aa3a78a63c413ddb5037151f375a26d44bae4f4e
shockliang <shockwater@gmail.com>
2020-07-29 00:35:15 +0800

    Editing action function.

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -26,11 +26,17 @@ const addBlogPost = (dispatch) => {
 
 const deleteBlogPost = (dispatch) => {
   return (id) => {
-    dispatch({type: 'delete_blogpost', payload: id})
-  }
+    dispatch({type: 'delete_blogpost', payload: id});
+  };
 };
+
+const editBlogPost = (dispatch) => {
+  return (id, title, content) => {
+    dispatch({type: 'edit_blogpost', payload: {id, title, content}});
+  };
+}
 
 export const {Context, Provider} = createDataContext(
   blogReducer,
-  {addBlogPost, deleteBlogPost},
+  {addBlogPost, deleteBlogPost, editBlogPost},
   [{title: 'Test Post', content: 'Test Content', id: 1}]);
--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -5,12 +5,15 @@ import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({navigation}) => {
   const id = navigation.getParam('id');
-  const {state} = useContext(Context);
+  const {state, editBlogPost} = useContext(Context);
   const blogPost = state.find(blogPost => blogPost.id === id);
 
   return (
     <BlogPostForm
       initialValues={{title: blogPost.title, content: blogPost.content}}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content);
+      }}
     />);
 };
 
