f8f681af762bda98a11e2e5877f1a969d974963b
shockliang <shockwater@gmail.com>
2020-08-06 00:10:47 +0800

    Setup mongo db.

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+const mongoUri = `mongodb+srv://track:${process.env.DB_PASSWORD}@track.g6ido.mongodb.net/trackDb?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.log('Error connection to mongo', err);
+})
 
 app.get('/', (req, res) => {
   res.send('Hi');
@@ -9,3 +26,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
+// track:HW7rpchky42b7Lho
