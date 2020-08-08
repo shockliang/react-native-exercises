a9cbb1d961ea49223591759902a9ed0e54127395
shockliang <shockwater@gmail.com>
2020-08-08 15:36:17 +0800

    Creating a jwt.

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
+  const {email, password} = req.body;
+
   try {
-    const {email, password} = req.body;
     const user = new User({email, password});
     await user.save();
-    res.send('post to signup');
+    const token = jwt.sign({userId: user._id}, 'my-secret-key');
+    res.send({token});
   } catch (err) {
     return res.status(422).send(err.message);
   }
