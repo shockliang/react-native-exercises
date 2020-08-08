6f3bb6ff83656063d45f2ded2547acbc57db9868
shockliang <shockwater@gmail.com>
2020-08-08 15:07:23 +0800

    Creating and saving a user.

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const moongose = require('mongoose');
+const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+  const {email, password } = req.body;
+  const user = new User({email, password});
+  await user.save();
   res.send('post to signup');
 });
 
