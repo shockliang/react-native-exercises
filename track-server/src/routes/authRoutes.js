4154c4e773f3313ea7607681bffdc1816e872ee8
shockliang <shockwater@gmail.com>
2020-08-11 00:19:53 +0800

    Adding signin route.

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -14,8 +14,32 @@ router.post('/signup', async (req, res) => {
     const token = jwt.sign({userId: user._id}, 'my-secret-key');
     res.send({token});
   } catch (err) {
+    console.log(err);
     return res.status(422).send(err.message);
   }
 });
+
+router.post('/signin', async (req, res) => {
+  const {email, password} = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({error: "Must provide email and password"});
+  }
+
+  const user = await User.findOne({email});
+  if (!user) {
+    return res.status(422).send({error: 'Invalid email or password'});
+  }
+
+  try {
+    await user.comparePassword(user.password);
+    const token = jwt.sign({userId: user._id}, 'my-secret-key');
+    res.send({token});
+  } catch (err) {
+    return res.status(422).send({error: 'Invalid email or password'});
+  }
+
+
+})
 
 module.exports = router;
