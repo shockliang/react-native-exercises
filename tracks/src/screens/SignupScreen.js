69e60c8eef4a92d1c2e1a47a4db643dc7afa041a
shockliang <shockwater@gmail.com>
2020-08-30 00:00:46 +0800

    Initial track client project.

---
+++ tracks/src/screens/SignupScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const SignupScreen = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 48}}>SignupScreen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.Create({});
+
+export default SignupScreen;
